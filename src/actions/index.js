eadaacd9efe8f99298c45f6735f76a1b3aa23e27
David Gilbert <d_gilbert5@yahoo.com>
2018-08-05 22:21:58 -0400

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const FETCH_POSTS = "fetch_posts";
+export const FETCH_POST = "fetch_post";
+export const CREATE_POST = "create_post";
+export const DELETE_POST = "delete_post";
+
+const ROOT_URL = "http://jsonplaceholder.typicode.com";
+
+export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts`);
+
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(values, callback) {
+  console.log("This is the post", values);
+  const request = axios
+    .post(`${ROOT_URL}/posts`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  axios
+    .delete(`${ROOT_URL}/posts/${id}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
+  };
+}
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+
+import reducers from "./reducers";
+import PostsIndex from "./components/posts_index";
+import PostsNew from "./components/posts_new";
+import PostsShow from "./components/posts_show";
+
+// import registerServiceWorker from './registerServiceWorker';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div style={{ padding: 25 }}>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+document.getElementById('root')
+);
+
+//registerServiceWorker();
